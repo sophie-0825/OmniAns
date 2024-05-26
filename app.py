99f597b9e1b83a79613229e75d7961ad7b01811d
Undefeated0725 <95053349+Undefeated0725@users.noreply.github.com>
2024-05-26 20:38:27 +0100

    update

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import glob
 import os
 from asyncflows import AsyncFlows
+from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+from starlette.responses import RedirectResponse
 
 class PDFChatbot:
-    def __init__(self, document_folder='documents', config_file='chatbot.yaml'):
+    def __init__(self, document_folder='documents', config_file='omniAns.yaml'):
         self.document_folder = document_folder
         self.config_file = config_file
         self.flow = None
@@ -29,6 +34,27 @@ class PDFChatbot:
         ])
         return result
 
-# Usage
-# chatbot = PDFChatbot()
-# result = await chatbot.ask("Your question")
+app = FastAPI()
+chatbot = PDFChatbot()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/upload/")
+async def upload_file(file: UploadFile = File(...)):
+    file_location = os.path.join(chatbot.document_folder, file.filename)
+    with open(file_location, "wb+") as file_object:
+        file_object.write(file.file.read())
+    chatbot.load_documents()
+    return RedirectResponse(url="/", status_code=303)
+
+@app.post("/ask/")
+async def ask_question(question: str = Form(...)):
+    response = await chatbot.ask(question)
+    return {"response": response}
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
